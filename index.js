e48b6ea703a3955acdb01dc79175ba1f60631f0b
Dylan <dylancarver14@gmail.com>
2022-07-07 22:47:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "Dylan Carver is the champion"
+newHeader.id = "victory"
+document.body.append(newHeader);
+
+
+//const ul = document.createElement("ul");
+//document.h1.append(ul);
+//const li = document.createElement("li");
+//document.h1.append(li);
